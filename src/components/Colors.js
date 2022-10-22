49a09b4602347e7a06b79d9fdfe972d28c7a35e2
ZeckeyBay <zfatihturan@gmail.com>
2022-10-22 11:25:16 +0300

    add element to array

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -2,13 +2,16 @@ import React,{useState} from "react";
 
 function Colors(){
     const [colors,setColors] = useState(["Red","Blue","Green"]);
-
+    const handleClick = () => {
+        setColors([...colors,"Gri"])
+    }
     return(
         <div>
         <h1>Renkler</h1>
         {colors.map((color,i)=> (
         <div key={i}>{color}</div>
         ))}
+        <button onClick={handleClick}>Ekle</button>
         </div>
     )
 }
